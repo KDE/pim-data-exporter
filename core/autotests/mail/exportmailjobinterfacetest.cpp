43a37241c92e6b39b353bbc11877a529b7a5a7be
Laurent Montel <montel@kde.org>
2020-04-29 07:52:44 +0200

    Implement export mail autotest

---
+++ core/autotests/mail/exportmailjobinterfacetest.cpp
@@ -0,0 +1,94 @@
+/*
+   Copyright (C) 2020 Laurent Montel <montel@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "exportmailjobinterfacetest.h"
+#include "archivestorage.h"
+#include "../resourceconvertertest.h"
+#include <QDebug>
+#include <QTest>
+#include <QSignalSpy>
+
+QTEST_MAIN(ExportMailJobInterfaceTest)
+
+ExportMailJobInterfaceTestImpl::ExportMailJobInterfaceTestImpl(QObject *parent, Utils::StoredTypes typeSelected, ArchiveStorage *archiveStorage, int numberOfStep)
+    : ExportMailJobInterface(parent, typeSelected, archiveStorage, numberOfStep)
+{
+
+}
+
+ExportMailJobInterfaceTestImpl::~ExportMailJobInterfaceTestImpl()
+{
+}
+
+void ExportMailJobInterfaceTestImpl::exportArchiveResource()
+{
+    qDebug() << " not implement yet";
+}
+
+void ExportMailJobInterfaceTestImpl::convertCollectionToRealPath(KConfigGroup &group, const QString &currentKey)
+{
+    ResourceConverterTest resourceConverter;
+    resourceConverter.convertCollectionToRealPath(group, currentKey);
+}
+
+void ExportMailJobInterfaceTestImpl::convertCollectionListToRealPath(KConfigGroup &group, const QString &currentKey)
+{
+    ResourceConverterTest resourceConverter;
+    resourceConverter.convertCollectionListToRealPath(group, currentKey);
+}
+
+void ExportMailJobInterfaceTestImpl::convertCollectionIdsToRealPath(KConfigGroup &group, const QString &currentKey, const QString &prefixCollection)
+{
+    ResourceConverterTest resourceConverter;
+    resourceConverter.convertCollectionIdsToRealPath(group, currentKey, prefixCollection);
+}
+
+void ExportMailJobInterfaceTestImpl::backupResources()
+{
+    qDebug() << " not implement yet";
+}
+
+ExportMailJobInterfaceTest::ExportMailJobInterfaceTest(QObject *parent)
+    : QObject(parent)
+{
+}
+
+void ExportMailJobInterfaceTest::exportMailConfigTest1()
+{
+    //Don't use setTestModeEnabled otherwise we can set env
+    //QStandardPaths::setTestModeEnabled(true);
+    qputenv("XDG_DATA_HOME", PIMDATAEXPORTER_DIR "/test1/share");
+    qputenv("XDG_CONFIG_HOME", PIMDATAEXPORTER_DIR "/test1/config");
+
+    //TODO fix file name.
+    const QString storeArchivePath(QStringLiteral("/tmp/foo.zip"));
+    ArchiveStorage *archiveStorage = new ArchiveStorage(storeArchivePath, this);
+    Q_ASSERT(archiveStorage->openArchive(true));
+    Utils::addVersion(archiveStorage->archive());
+    Utils::storeDataExportInfo(storeArchivePath, archiveStorage->archive());
+
+    ExportMailJobInterfaceTestImpl *exportNote = new ExportMailJobInterfaceTestImpl(this, {Utils::StoredType::Config}, archiveStorage, 1);
+    QSignalSpy finish(exportNote, &ExportMailJobInterfaceTestImpl::jobFinished);
+    QSignalSpy error(exportNote, &ExportMailJobInterfaceTestImpl::error);
+    exportNote->start();
+    QVERIFY(finish.wait());
+    QCOMPARE(error.count(), 0);
+    delete exportNote;
+    delete archiveStorage;
+}
---
+++ core/autotests/mail/exportmailjobinterfacetest.h
@@ -0,0 +1,56 @@
+/*
+   Copyright (C) 2020 Laurent Montel <montel@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef EXPORTMAILJOBINTERFACETEST_H
+#define EXPORTMAILJOBINTERFACETEST_H
+
+#include <QObject>
+
+#include "mail/exportmailjobinterface.h"
+
+class ExportMailJobInterfaceTestImpl : public ExportMailJobInterface
+{
+    Q_OBJECT
+public:
+    explicit ExportMailJobInterfaceTestImpl(QObject *parent, Utils::StoredTypes typeSelected, ArchiveStorage *archiveStorage, int numberOfStep);
+    ~ExportMailJobInterfaceTestImpl();
+
+protected:
+    void exportArchiveResource() override;
+
+protected:
+    void convertCollectionToRealPath(KConfigGroup &group, const QString &currentKey) override;
+    void convertCollectionListToRealPath(KConfigGroup &group, const QString &currentKey) override;
+
+    void convertCollectionIdsToRealPath(KConfigGroup &group, const QString &currentKey, const QString &prefixCollection) override;
+    void backupResources() override;
+};
+
+
+class ExportMailJobInterfaceTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ExportMailJobInterfaceTest(QObject *parent = nullptr);
+    ~ExportMailJobInterfaceTest() = default;
+private Q_SLOTS:
+    void exportMailConfigTest1();
+};
+
+#endif // EXPORTMAILJOBINTERFACETEST_H
