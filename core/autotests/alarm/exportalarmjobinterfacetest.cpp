148b83cedd1b1471bdfde0bfb50cabf8c80533ab
Laurent Montel <montel@kde.org>
2020-04-29 07:40:53 +0200

    Add alarm autotest support

---
+++ core/autotests/alarm/exportalarmjobinterfacetest.cpp
@@ -0,0 +1,75 @@
+/*
+   Copyright (C) 2020 Laurent Montel <montel@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "exportalarmjobinterfacetest.h"
+#include "archivestorage.h"
+#include "../resourceconvertertest.h"
+#include <QDebug>
+#include <QTest>
+#include <QSignalSpy>
+
+QTEST_MAIN(ExportAlarmJobInterfaceTest)
+
+ExportAlarmJobInterfaceTestImpl::ExportAlarmJobInterfaceTestImpl(QObject *parent, Utils::StoredTypes typeSelected, ArchiveStorage *archiveStorage, int numberOfStep)
+    : ExportAlarmJobInterface(parent, typeSelected, archiveStorage, numberOfStep)
+{
+
+}
+
+ExportAlarmJobInterfaceTestImpl::~ExportAlarmJobInterfaceTestImpl()
+{
+}
+
+void ExportAlarmJobInterfaceTestImpl::exportArchiveResource()
+{
+    qDebug() << " not implement yet";
+}
+
+ExportAlarmJobInterfaceTest::ExportAlarmJobInterfaceTest(QObject *parent)
+    : QObject(parent)
+{
+}
+
+void ExportAlarmJobInterfaceTest::importAlarmConfigTest1()
+{
+    //Don't use setTestModeEnabled otherwise we can set env
+    //QStandardPaths::setTestModeEnabled(true);
+    qputenv("XDG_DATA_HOME", PIMDATAEXPORTER_DIR "/test1/share");
+    qputenv("XDG_CONFIG_HOME", PIMDATAEXPORTER_DIR "/test1/config");
+
+    //TODO fix file name.
+    const QString storeArchivePath(QStringLiteral("/tmp/foo.zip"));
+    ArchiveStorage *archiveStorage = new ArchiveStorage(storeArchivePath, this);
+    Q_ASSERT(archiveStorage->openArchive(true));
+    Utils::addVersion(archiveStorage->archive());
+    Utils::storeDataExportInfo(storeArchivePath, archiveStorage->archive());
+
+    ExportAlarmJobInterfaceTestImpl *exportNote = new ExportAlarmJobInterfaceTestImpl(this, {Utils::StoredType::Config}, archiveStorage, 1);
+    QSignalSpy finish(exportNote, &ExportAlarmJobInterfaceTestImpl::jobFinished);
+    QSignalSpy error(exportNote, &ExportAlarmJobInterfaceTestImpl::error);
+    exportNote->start();
+    QVERIFY(finish.wait());
+    QCOMPARE(error.count(), 0);
+    delete exportNote;
+    delete archiveStorage;
+}
+
+
+
+
---
+++ core/autotests/alarm/exportalarmjobinterfacetest.h
@@ -0,0 +1,49 @@
+/*
+   Copyright (C) 2020 Laurent Montel <montel@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef ExportAlarmJobInterfaceTest_H
+#define ExportAlarmJobInterfaceTest_H
+
+#include <QObject>
+
+#include "alarm/exportalarmjobinterface.h"
+
+class ExportAlarmJobInterfaceTestImpl : public ExportAlarmJobInterface
+{
+    Q_OBJECT
+public:
+    explicit ExportAlarmJobInterfaceTestImpl(QObject *parent, Utils::StoredTypes typeSelected, ArchiveStorage *archiveStorage, int numberOfStep);
+    ~ExportAlarmJobInterfaceTestImpl();
+
+protected:
+    void exportArchiveResource() override;
+};
+
+
+class ExportAlarmJobInterfaceTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ExportAlarmJobInterfaceTest(QObject *parent = nullptr);
+    ~ExportAlarmJobInterfaceTest() = default;
+private Q_SLOTS:
+    void importAlarmConfigTest1();
+};
+
+#endif // ExportAlarmJobInterfaceTest_H
