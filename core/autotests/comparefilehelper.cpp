83fb0a01dcf8e323ffdd4bba1f6bbc6c6ac17710
Laurent Montel <montel@kde.org>
2020-05-22 07:52:04 +0200

    Make working on windows too

--- core/autotests/comparefilehelper.cpp
+++ core/autotests/comparefilehelper.cpp
@@ -24,14 +24,35 @@
 
 void CompareFileHelper::compareFile(const QString &referenceFile, const QString &archiveFile)
 {
-    //qDebug() << "referenceFile " << referenceFile << " archiveFile " << archiveFile;
     QProcess proc;
-    const QStringList args = QStringList()
-                             << QStringLiteral("-u")
-                             << referenceFile
-                             << archiveFile;
+    //qDebug() << "referenceFile " << referenceFile << " archiveFile " << archiveFile;
+#ifdef _WIN32
+    QStringList args = QStringList()
+            << QStringLiteral("Compare-Object")
+            << QString(QStringLiteral("(Get-Content %1)")).arg(referenceFile)
+            << QString(QStringLiteral("(Get-Content %1)")).arg(archiveFile);
+
+    proc.start(QStringLiteral("powershell"), args);
+    QVERIFY(proc.waitForFinished());
+
+    auto pStdOut = proc.readAllStandardOutput();
+    if (pStdOut.size()) {
+        qDebug() << "Files are different, diff output message:\n"
+                 << pStdOut.toStdString().c_str();
+    }
+
+    QCOMPARE(pStdOut.size(), 0);
+#else
+    // compare to reference file
+    QStringList args = QStringList()
+            << QStringLiteral("-u")
+            << referenceFile
+            << archiveFile;
+
     proc.setProcessChannelMode(QProcess::ForwardedChannels);
     proc.start(QStringLiteral("diff"), args);
     QVERIFY(proc.waitForFinished());
     QCOMPARE(proc.exitCode(), 0);
+#endif
+
 }
