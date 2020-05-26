e2cc5024e2d874be967ad75bc02f1d558078ed38
Laurent Montel <montel@kde.org>
2020-05-26 13:37:44 +0200

    Try to abstract mailtransport

--- core/mail/importmailjobinterface.h
+++ core/mail/importmailjobinterface.h
@@ -36,6 +36,67 @@ class Transport;
 namespace MailCommon {
 class MailFilter;
 }
+
+class PIMDATAEXPORTER_TESTS_EXPORT SmtpMailTransport
+{
+    explicit SmtpMailTransport(const KConfigGroup &group);
+
+public:
+    Q_REQUIRED_RESULT QString name() const;
+    void setName(const QString &name);
+
+    Q_REQUIRED_RESULT QString host() const;
+    void setHost(const QString &host);
+
+    Q_REQUIRED_RESULT int port() const;
+    void setPort(int port);
+
+    Q_REQUIRED_RESULT QString userName() const;
+    void setUserName(const QString &userName);
+
+    Q_REQUIRED_RESULT QString precommand() const;
+    void setPrecommand(const QString &precommand);
+
+    Q_REQUIRED_RESULT bool requiresAuthentication() const;
+    void setRequiresAuthentication(bool requiresAuthentication);
+
+    Q_REQUIRED_RESULT bool specifyHostname() const;
+    void setSpecifyHostname(bool specifyHostname);
+
+    Q_REQUIRED_RESULT QString localHostname() const;
+    void setLocalHostname(const QString &localHostname);
+
+    Q_REQUIRED_RESULT bool specifySenderOverwriteAddress() const;
+    void setSpecifySenderOverwriteAddress(bool specifySenderOverwriteAddress);
+
+    Q_REQUIRED_RESULT bool storePassword() const;
+    void setStorePassword(bool storePassword);
+
+    Q_REQUIRED_RESULT QString senderOverwriteAddress() const;
+    void setSenderOverwriteAddress(const QString &senderOverwriteAddress);
+
+    Q_REQUIRED_RESULT int encryption() const;
+    void setEncryption(int encryption);
+
+    Q_REQUIRED_RESULT int authenticationType() const;
+    void setAuthenticationType(int authenticationType);
+
+private:
+    QString mName;
+    QString mHost;
+    QString mUserName;
+    QString mPrecommand;
+    QString mLocalHostname;
+    QString mSenderOverwriteAddress;
+    int mPort = -1;
+    int mEncryption = -1;
+    int mAuthenticationType = 1;
+    bool mRequiresAuthentication = false;
+    bool mSpecifyHostname = false;
+    bool mSpecifySenderOverwriteAddress = false;
+    bool mStorePassword = false;
+};
+
 class PIMDATAEXPORTER_TESTS_EXPORT ImportMailJobInterface : public AbstractImportExportJob
 {
     Q_OBJECT
