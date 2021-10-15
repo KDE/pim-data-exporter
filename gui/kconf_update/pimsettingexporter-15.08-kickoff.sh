0e75fb49343ba61abb652feaac5168a7fb359dc6
Laurent Montel <montel@kde.org>
2021-10-15 06:52:47 +0200

    GIT_SILENT: remove old kconf_update

--- gui/kconf_update/pimsettingexporter-15.08-kickoff.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-kickoffrcname=`qtpaths --locate-file  GenericConfigLocation kickoffrc`
-if [ -f "$kickoffrcname" ]; then
-   sed -i "s/\/pimsettingexporter.desktop/\/org.kde.pimsettingexporter.desktop/" $kickoffrcname
-fi
