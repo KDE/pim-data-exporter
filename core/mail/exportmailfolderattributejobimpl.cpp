7290bdb9babcf85b799aa5768b830e52de837d0b
Laurent Montel <montel@kde.org>
2022-05-20 13:50:11 +0200

    Add impl job (so we can autotest it)

---
+++ core/mail/exportmailfolderattributejobimpl.cpp
@@ -0,0 +1,63 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "exportmailfolderattributejobimpl.h"
+#include "pimdataexportcore_debug.h"
+#include <Akonadi/CollectionFetchJob>
+#include <Akonadi/CollectionFetchScope>
+#include <Akonadi/EntityDisplayAttribute>
+#include <MailCommon/ExpireCollectionAttribute>
+
+ExportMailFolderAttributeJobImpl::ExportMailFolderAttributeJobImpl(QObject *parent)
+    : ExportMailFolderAttributeJob{parent}
+{
+}
+
+ExportMailFolderAttributeJobImpl::~ExportMailFolderAttributeJobImpl() = default;
+
+void ExportMailFolderAttributeJobImpl::fetchAttributes()
+{
+    auto job = new Akonadi::CollectionFetchJob(Akonadi::Collection::root(), Akonadi::CollectionFetchJob::Recursive, this);
+    job->fetchScope().fetchAttribute<MailCommon::ExpireCollectionAttribute>();
+    job->fetchScope().fetchAttribute<Akonadi::EntityDisplayAttribute>();
+    job->fetchScope().setContentMimeTypes({QStringLiteral("message/rfc822")});
+    connect(job, &Akonadi::CollectionFetchJob::result, this, &ExportMailFolderAttributeJobImpl::slotFetchFinished);
+}
+
+void ExportMailFolderAttributeJobImpl::slotFetchFinished(KJob *job)
+{
+    if (job->error()) {
+        Q_EMIT failed();
+        deleteLater();
+        return;
+    }
+
+    auto list = static_cast<Akonadi::CollectionFetchJob *>(job);
+    const Akonadi::Collection::List cols = list->collections();
+    if (cols.isEmpty()) {
+        Q_EMIT successed();
+        qCWarning(PIMDATAEXPORTERCORE_LOG) << "It seems wierd that there is not collection.";
+        deleteLater();
+        return;
+    }
+    QMap<QString, AttributeInfo> lstAttributeInfo;
+    for (const auto &col : cols) {
+        AttributeInfo info;
+
+        const auto *attr = col.attribute<MailCommon::ExpireCollectionAttribute>();
+        if (attr) {
+            info.expireAttribute = attr->serialized();
+        }
+        const auto *attrDisplay = col.attribute<Akonadi::EntityDisplayAttribute>();
+        if (attrDisplay) {
+            info.expireAttribute = attrDisplay->serialized();
+        }
+        if (info.isValid()) {
+            lstAttributeInfo.insert(QString::number(col.id()), info);
+        }
+    }
+    storeFileFolderAttribute(lstAttributeInfo);
+}
---
+++ core/mail/exportmailfolderattributejobimpl.h
@@ -0,0 +1,23 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "exportmailfolderattributejob.h"
+
+class ExportMailFolderAttributeJobImpl : public ExportMailFolderAttributeJob
+{
+    Q_OBJECT
+public:
+    explicit ExportMailFolderAttributeJobImpl(QObject *parent = nullptr);
+    ~ExportMailFolderAttributeJobImpl() override;
+
+protected:
+    void fetchAttributes() override final;
+
+private:
+    void slotFetchFinished(KJob *job);
+};
