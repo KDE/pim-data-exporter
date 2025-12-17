91093513ef12d3995f5984e03a59975400fc2da5
Laurent Montel <montel@kde.org>
2025-12-17 08:08:06 +0100

    Remove duplicate headers

--- core/mail/exportmailjobinterfaceimpl.cpp
+++ core/mail/exportmailjobinterfaceimpl.cpp
@@ -8,7 +8,6 @@
 using namespace Qt::Literals::StringLiterals;
 
 #include "exportmailfolderattributejobimpl.h"
-#include <KIdentityManagementCore/IdentityManager>
 #include <MailCommon/FilterImporterExporter>
 #include <MailCommon/FilterManager>
 #include <MailCommon/MailUtil>
